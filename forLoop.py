f2b953f47206fe9de6fd427b54aa124875c6d00d
Jun seok Lee <57342182+junseokieo@users.noreply.github.com>
2020-12-28 18:58:44 +0900

    Add files via upload
    Python tutorial

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+days = ('Mon', 'Tue', 'Wed','Thu','Fri')
+
+for weeks in days:
+    if weeks is "Wed":
+        break
+    else:
+        print(weeks)
+
+for letter in "junseok":
+    print(letter)
+import math
+print(math.ceil(1.45))
