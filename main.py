3746710bb4b3df17997d31453ccd5a1588df8140
Jun seok Lee <57342182+junseokieo@users.noreply.github.com>
2020-12-30 00:46:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from pip._vendor import requests
+import requests
 from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+#html 가져오기 성공..
 
-pagination = indeed_soup.find("div", {"class": "pagination"})
+# bring page numbers from beautiful soup
+
+indeed_soup = BeautifulSoup(indeed_result.text,'html.parser')
+
+pagination = indeed_soup.find("div", {"class":"pagination"})
 
 pages = pagination.find_all('a')
+
 spans = []
 for page in pages:
-    spans.append(page.find("span"))
-spans = spans[:-1]
+  spans.append(page.find("span").string)
+print(spans[:-1])
